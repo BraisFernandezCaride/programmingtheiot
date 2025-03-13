5a587b90efa18c9947fb3ca27556187826b7979e
BraisFernandezCaride <brais.fernandez.caride@gmail.com>
2025-03-13 09:42:52 +0100

    lab03

--- src/main/python/programmingtheiot/cda/app/ConstrainedDeviceApp.py
+++ src/main/python/programmingtheiot/cda/app/ConstrainedDeviceApp.py
@@ -16,6 +16,11 @@ from programmingtheiot.cda.system.SystemPerformanceManager import SystemPerforma
 
 from time import sleep
 
+import programmingtheiot.common.ConfigConst as ConfigConst
+
+from programmingtheiot.common.ConfigUtil import ConfigUtil
+from programmingtheiot.cda.app.DeviceDataManager import DeviceDataManager
+
 logging.basicConfig(format = '%(asctime)s:%(name)s:%(levelname)s:%(message)s', level = logging.DEBUG)
 
 class ConstrainedDeviceApp():
@@ -25,42 +30,26 @@ class ConstrainedDeviceApp():
 	"""
 	
 	def __init__(self):
-		"""
-		Initialization of class.
-		
-		@param path The name of the resource to apply to the URI.
-		"""
 		logging.info("Initializing CDA...")
 
-		self.sysPerfMgr = SystemPerformanceManager()
-		
-		# "TODO": implementation here
+		self.dataMgr = DeviceDataManager()
 
 	def startApp(self):
-		"""
-		Start the CDA. Calls startManager() on the device data manager instance.
-		
-		"""
 		logging.info("Starting CDA...")
-		
-		self.sysPerfMgr.startManager()
-		
-		# "TODO": implementation here
-		
+
+		self.dataMgr.startManager()
+
 		logging.info("CDA started.")
 
 	def stopApp(self, code: int):
-		"""
-		Stop the CDA. Calls stopManager() on the device data manager instance.
-		
-		"""
 		logging.info("CDA stopping...")
-		
-		self.sysPerfMgr.stopManager()
 
-		# "TODO": implementation here
-		
+		self.dataMgr.stopManager()
+
 		logging.info("CDA stopped with exit code %s.", str(code))
+
+
+
 		
 	def parseArgs(self, args):
 		"""
@@ -72,19 +61,19 @@ class ConstrainedDeviceApp():
 
 
 def main():
-	"""
-	Main function definition for running client as application.
-	
-	Current implementation runs for 35 seconds then exits.
-	"""
 	cda = ConstrainedDeviceApp()
 	cda.startApp()
-	
-	# run for 10 seconds - this can be changed as needed
-	sleep(10)
-	
-	# optionally stop the app - this can be removed if needed
-	cda.stopApp(0)
+
+	runForever = ConfigUtil().getBoolean(ConfigConst.CONSTRAINED_DEVICE, ConfigConst.RUN_FOREVER_KEY)
+
+	if runForever:
+		while (True):
+			sleep(5)
+
+	else:
+		# TODO: Make the '65' value configurable
+		sleep(65)
+		cda.stopApp(0)
 
 if __name__ == '__main__':
 	"""
