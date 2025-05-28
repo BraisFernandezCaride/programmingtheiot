4d52240d9bec39b8c6ec45c25b3954f4302262d7
BraisFernandezCaride <brais.fernandez.caride@gmail.com>
2025-05-28 16:08:55 +0200

    lab09.1

--- src/main/python/programmingtheiot/cda/connection/CoapClientConnector.py
+++ src/main/python/programmingtheiot/cda/connection/CoapClientConnector.py
@@ -9,6 +9,7 @@
 
 import logging
 import socket
+import traceback
 
 import programmingtheiot.common.ConfigConst as ConfigConst
 
@@ -18,6 +19,18 @@ from programmingtheiot.common.ResourceNameEnum import ResourceNameEnum
 
 from programmingtheiot.common.IDataMessageListener import IDataMessageListener
 from programmingtheiot.cda.connection.IRequestResponseClient import IRequestResponseClient
+from programmingtheiot.cda.connection.CoapClientConnector import CoapClientConnector
+
+from programmingtheiot.data.DataUtil import DataUtil
+
+import asyncio
+
+from aiocoap import *
+
+from coapthon import defines
+from coapthon.client.helperclient import HelperClient
+from coapthon.utils import parse_uri
+from coapthon.utils import generate_random_token
 
 class CoapClientConnector(IRequestResponseClient):
 	"""
@@ -25,9 +38,42 @@ class CoapClientConnector(IRequestResponseClient):
 	
 	"""
 	
-	def __init__(self):
-		pass
-	
+	def __init__(self, dataMsgListener: IDataMessageListener = None):
+		self.config = ConfigUtil()
+		self.dataMsgListener = dataMsgListener
+		self.enableConfirmedMsgs = False
+		self.coapClient = None
+
+		self.observeRequests = {}
+
+		self.host = self.config.getProperty(
+			ConfigConst.COAP_GATEWAY_SERVICE,
+			ConfigConst.HOST_KEY,
+			ConfigConst.DEFAULT_HOST
+		)
+		self.port = self.config.getInteger(
+			ConfigConst.COAP_GATEWAY_SERVICE,
+			ConfigConst.PORT_KEY,
+			ConfigConst.DEFAULT_COAP_PORT
+		)
+		self.uriPath = "coap://" + self.host + ":" + str(self.port) + "/"
+
+		logging.info('\tHost:Port: %s:%s', self.host, str(self.port))
+
+		self.includeDebugLogDetail = True
+
+		try:
+			tmpHost = socket.gethostbyname(self.host)
+
+			if tmpHost:
+				self.host = tmpHost
+				self._initClient()
+			else:
+				logging.error("Can't resolve host: " + self.host)
+
+		except socket.gaierror:
+			logging.info("Failed to resolve host: " + self.host)
+
 	def sendDiscoveryRequest(self, timeout: int = IRequestResponseClient.DEFAULT_TIMEOUT) -> bool:
 		pass
 
@@ -51,6 +97,19 @@ class CoapClientConnector(IRequestResponseClient):
 
 	def stopObserver(self, resource: ResourceNameEnum = None, name: str = None, timeout: int = IRequestResponseClient.DEFAULT_TIMEOUT) -> bool:
 		pass
-	
+
 	def _initClient(self):
-		pass
+		asyncio.get_event_loop().run_until_complete(self._initClientContext())
+
+	async def _initClientContext(self):
+		try:
+			logging.info("Creating CoAP client for URI path: " + self.uriPath)
+
+			self.coapClient = await Context.create_client_context()
+
+			logging.info('Client context created. Will invoke resources at: ' + self.uriPath)
+
+		except Exception as e:
+			# obviously, this is a critical failure - you may want to handle this differently
+			logging.error("Failed to create CoAP client to URI path: " + self.uriPath)
+			traceback.print_exception(type(e), e, e.__traceback__)
