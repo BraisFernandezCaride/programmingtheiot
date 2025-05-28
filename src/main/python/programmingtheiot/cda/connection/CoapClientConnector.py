0aacb25dcee4eee0cd74475adf17b262590c2c2c
BraisFernandezCaride <brais.fernandez.caride@gmail.com>
2025-05-28 16:38:48 +0200

    lab09.3

--- src/main/python/programmingtheiot/cda/connection/CoapClientConnector.py
+++ src/main/python/programmingtheiot/cda/connection/CoapClientConnector.py
@@ -147,8 +147,53 @@ class CoapClientConnector(IRequestResponseClient):
 	def sendPostRequest(self, resource: ResourceNameEnum = None, name: str = None, enableCON: bool = False, payload: str = None, timeout: int = IRequestResponseClient.DEFAULT_TIMEOUT) -> bool:
 		pass
 
-	def sendPutRequest(self, resource: ResourceNameEnum = None, name: str = None, enableCON: bool = False, payload: str = None, timeout: int = IRequestResponseClient.DEFAULT_TIMEOUT) -> bool:
-		pass
+	def sendPutRequest(
+		self,
+		resource: ResourceNameEnum = None,
+		name: str = None,
+		enableCON: bool = False,
+		payload: str = None,
+		timeout: int = IRequestResponseClient.DEFAULT_TIMEOUT
+		) -> bool:
+		if resource or name:
+			resourcePath = self._createResourcePath(resource, name)
+
+			logging.info("Issuing Async PUT to path: " + resourcePath)
+
+			asyncio.get_event_loop().run_until_complete(
+				self._handlePutRequest(
+					resourcePath=resourcePath,
+					payload=payload,
+					enableCON=enableCON
+				)
+			)
+		else:
+			logging.warning("Can't issue Async PUT - no path or path list provided.")
+	
+	async def _handlePutRequest(self, resourcePath: str = None, payload: str = None, enableCON: bool = False):
+		try:
+			msgType = NON
+
+			if enableCON:
+				msgType = CON
+
+			payloadBytes = b''
+
+			# Decide which encoding to use - can also load from config
+			if payload:
+				payloadBytes = payload.encode('utf-8')
+
+			msg = Message(mtype=msgType, payload=payloadBytes, code=Code.PUT, uri=resourcePath)
+			req = self.coapClient.request(msg)
+			responseData = await req.response
+
+			self._onPutResponse(responseData)
+
+		except Exception as e:
+			# TODO: for debugging, you may want to optionally include the stack trace, as shown
+			logging.warning("Failed to process PUT request for path: " + str(resourcePath))
+			traceback.print_exception(type(e), e, e.__traceback__)
+
 
 	def setDataMessageListener(self, listener: IDataMessageListener = None) -> bool:
 		pass
