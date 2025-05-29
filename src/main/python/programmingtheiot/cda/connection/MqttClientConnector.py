235b7d3280f86242588fdaa085ea833198de06af
BraisFernandezCaride <brais.fernandez.caride@gmail.com>
2025-05-29 12:07:06 +0200

    lab10.3

--- src/main/python/programmingtheiot/cda/connection/MqttClientConnector.py
+++ src/main/python/programmingtheiot/cda/connection/MqttClientConnector.py
@@ -18,6 +18,7 @@ from programmingtheiot.common.IDataMessageListener import IDataMessageListener
 from programmingtheiot.common.ResourceNameEnum import ResourceNameEnum
 
 from programmingtheiot.cda.connection.IPubSubClient import IPubSubClient
+from programmingtheiot.data.DataUtil import DataUtil
 
 class MqttClientConnector(IPubSubClient):
 	"""
@@ -129,7 +130,15 @@ class MqttClientConnector(IPubSubClient):
 			return False
 		
 	def onConnect(self, client, userdata, flags, rc):
-		logging.info('MQTT client connected to broker: ' + str(client))
+		logging.info('[Callback] Connected to MQTT broker. Result code: ' + str(rc))
+
+		# NOTE: Be sure to set `self.defaultQos` during instantiation!
+		self.mqttClient.subscribe( \
+			topic = ResourceNameEnum.CDA_ACTUATOR_CMD_RESOURCE.value, qos = self.defaultQos)
+
+		self.mqttClient.message_callback_add( \
+			sub = ResourceNameEnum.CDA_ACTUATOR_CMD_RESOURCE.value, \
+			callback = self.onActuatorCommandMessage)
 		
 	def onDisconnect(self, client, userdata, rc):
 		logging.info('MQTT client disconnected from broker: ' + str(client))
@@ -150,21 +159,19 @@ class MqttClientConnector(IPubSubClient):
 		logging.info('MQTT client subscribed: ' + str(client))
 	
 	def onActuatorCommandMessage(self, client, userdata, msg):
-		"""
-		This callback is defined as a convenience, but does not
-		need to be used and can be ignored.
-		
-		It's simply an example for how you can create your own
-		custom callback for incoming messages from a specific
-		topic subscription (such as for actuator commands).
-		
-		@param client The client reference context.
-		@param userdata The user reference context.
-		@param msg The message context, including the embedded payload.
-		"""
-		pass
+		logging.info('[Callback] Actuator command message received. Topic: %s.', msg.topic)
+
+		if self.dataMsgListener:
+			try:
+				# assumes all data is encoded using UTF-8 (between GDA and CDA)
+				actuatorData = DataUtil().jsonToActuatorData(msg.payload.decode('utf-8'))
+
+				self.dataMsgListener.handleActuatorCommandMessage(actuatorData)
+			except:
+				logging.exception("Failed to convert incoming actuation command payload to ActuatorData: ")
 	
 	def publishMessage(self, resource: ResourceNameEnum = None, msg: str = None, qos: int = ConfigConst.DEFAULT_QOS) -> bool:
+	
 		# check validity of resource (topic)
 		if not resource:
 			logging.warning('No topic specified. Cannot publish message.')
@@ -181,8 +188,12 @@ class MqttClientConnector(IPubSubClient):
 
 		# publish message, and wait for publish to complete before returning
 		msgInfo = self.mqttClient.publish(topic = resource.value, payload = msg, qos = qos)
-		msgInfo.wait_for_publish()
 
+		# The next SLOC is commented out now - recall it was added in Lab Module 06
+		#msgInfo.wait_for_publish()
+
+		# NOTE: The 'True' return no longer guarantees successful publish,
+		# as it will return before the publish may successfully complete
 		return True
 	
 	def subscribeToTopic(self, resource: ResourceNameEnum = None, callback = None, qos: int = ConfigConst.DEFAULT_QOS) -> bool:
@@ -212,5 +223,10 @@ class MqttClientConnector(IPubSubClient):
 		return True
 
 	def setDataMessageListener(self, listener: IDataMessageListener = None):
-    		if listener:
-        		self.dataMsgListener = listener
+		if listener:
+			self.dataMsgListener = listener
+
+		# TODO: Call this from within `DeviceDataManager's` constructor
+		if self.enableMqttClient:
+			self.mqttClient = MqttClientConnector()
+			self.mqttClient.setDataMessageListener(self)
